0a77c9e4df937f62db2deb0b9cb571058abfc804
vanthinh <ta642379@gmail.com>
2023-11-29 15:21:09 +0700

    update index template

--- app.py
+++ app.py
@@ -1,17 +1,29 @@
 from flask import Flask, render_template, request, jsonify
-from utils import model_predict
+from utils import model_predict, get_encoder_class
 app = Flask(__name__)
 
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    category_labels,branch_labels = get_encoder_class()
+    return render_template("index.html", category_labels = category_labels, branch_labels =branch_labels)
 
 @app.route('/predict', methods=['POST'])
 def predict():
+    category_labels,branch_labels = get_encoder_class()
+
     form_data = request.form.to_dict()
-    prediction = 123
-    return render_template("index.html", form_data=form_data, prediction=prediction)
+    # nhận diện giá bằng model đã train
+    branch_selected = request.form['branch']
+    category_selected = request.form['category']
+    prediction = model_predict(form_data)
+    return render_template("index.html", 
+                            form_data=form_data,
+                            prediction=prediction, 
+                            branch_selected=branch_selected,
+                            category_selected=category_selected,
+                            category_labels = category_labels, 
+                            branch_labels =branch_labels)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080, debug=True)
--- utils.py
+++ utils.py
@@ -1,13 +1,26 @@
 import pickle
+import pandas as pd
+import numpy as np
+import warnings
+warnings.filterwarnings("ignore", category=UserWarning)
 
-cv = pickle.load(open("models/cv.pkl", 'rb'))
-clf = pickle.load(open("models/clf.pkl", 'rb'))
+model = pickle.load(open("models/sneaker-prediction_random-forest.pkl", 'rb'))
+category_encoder = pickle.load(open("models/Category_encoder.pkl", 'rb'))
+branch_encoder = pickle.load(open("models/Branch_encoder.pkl", 'rb'))
 
-def model_predict(email):
-    if email == "":
-        return ""
-    tokenized_email = cv.transform([email]) # X 
-    prediction = clf.predict(tokenized_email)
-    # If the email is spam prediction should be 1
-    prediction = 1 if prediction == 1 else -1
-    return prediction
+def get_encoder_class():
+    return category_encoder.classes_ , branch_encoder.classes_
+
+def model_predict(input_data):
+    try:
+        new_input = input_data.copy()
+        new_input['category'] = category_encoder.transform([input_data['category']])
+        new_input['branch'] = branch_encoder.transform([input_data['branch']])
+        input_data_df = pd.DataFrame([new_input])
+        prediction = model.predict(input_data_df)
+    except Exception as e:
+        return 'Can not predict your data'
+    else:
+        return prediction
+
+    
